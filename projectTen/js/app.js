1aea89c7c7eecf2e24cda62408d4961972858644
Edu Martinez <edumartsil95@tutanota.com>
2025-06-12 22:47:25 +0200

    project Ten finished

---
+++ projectTen/js/app.js
@@ -0,0 +1,10 @@
+let mySwiper = new Swiper('.swiper-container', {
+    effect:'cube',
+    loop: true,
+    autoplay: {
+        delay: 3000,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
